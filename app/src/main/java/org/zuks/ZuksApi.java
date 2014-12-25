1af0bd15cdc6a28f98819dcc18039a760be761f9
Simon Seyer <seyer@redpeppix.de>
2014-12-26 00:49:52 +0100

    Applied API changes to App

--- app/src/main/java/org/zuks/ZuksApi.java
+++ app/src/main/java/org/zuks/ZuksApi.java
@@ -2,10 +2,13 @@ package org.zuks;
 
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.util.Log;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
@@ -24,91 +27,112 @@ public class ZuksApi {
     private static final String TAG = "ZuksApi";
     private static final String BASE_URL = "http://192.168.11.108:8000/";
 
-    public void updateUserLocation(int userID, Location location) {
+    public void updateUserLocation(final int userID, final Location location) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject volunteer = get(BASE_URL + "volunteers/" + userID + "/");
+                    if (volunteer != null) {
+
+                        JSONObject jsonLocation = new JSONObject();
+                        jsonLocation.put("latitude", location.getLatitude());
+                        jsonLocation.put("longitude", location.getLongitude());
+
+                        patch(BASE_URL + "locations/" + volunteer.getString("id") + "/", jsonLocation.toString());
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void patch(String url, String content) {
+        Log.i(TAG, "PATCH: '" + url + "': " + content);
+
+        StringEntity stringEntity = null;
         try {
-            JSONObject jsonLocation = new JSONObject();
-            jsonLocation.put("latitude", location.getLatitude());
-            jsonLocation.put("longitude", location.getLongitude());
+            stringEntity = new StringEntity(content);
+        } catch (UnsupportedEncodingException e) {
+            Log.w(TAG, e);
+        }
 
-            JSONObject jsonData = new JSONObject();
-            jsonData.put("location", jsonLocation);
+        HttpPatch patch = new HttpPatch(url);
+        patch.setEntity(stringEntity);
+        patch.setHeader("Accept", "application/json");
+        patch.setHeader("Content-type", "application/json");
 
-            ApiPatchCall call = new ApiPatchCall();
-            call.execute(BASE_URL + "volunteers/" + userID, jsonData.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
+        HttpResponse response = null;
+        try {
+            response = new DefaultHttpClient().execute(patch);
+        } catch (IOException e) {
+            Log.w(TAG, e);
+        }
+
+        try {
+            String resp = EntityUtils.toString(response.getEntity());
+            Log.i(TAG, "Response: " + resp);
+        } catch (IOException e) {
+            Log.w(TAG, e);
         }
     }
 
+    private JSONObject get(String url) {
+        Log.i(TAG, "GET: '" + url);
 
-    private class ApiPatchCall extends AsyncTask<String, String, Boolean> {
+        HttpGet patch = new HttpGet(url);
+        patch.setHeader("Accept", "application/json");
+        patch.setHeader("Content-type", "application/json");
+
+        HttpResponse response = null;
+        try {
+            response = new DefaultHttpClient().execute(patch);
+        } catch (IOException e) {
+            Log.w(TAG, e);
+        }
+
+        String resp = null;
+        try {
+            resp = EntityUtils.toString(response.getEntity());
+            Log.i(TAG, "Response: " + resp);
+        } catch (IOException e) {
+            Log.w(TAG, e);
+        }
+
+        try {
+            return new JSONObject(resp);
+        } catch (JSONException e) {
+            Log.w(TAG, e);
+        }
+
+        return null;
+    }
+
+    private class HttpPatch extends HttpEntityEnclosingRequestBase {
+
+        public final static String METHOD_NAME = "PATCH";
+
+        public HttpPatch() {
+            super();
+        }
+
+        public HttpPatch(final URI uri) {
+            super();
+            setURI(uri);
+        }
+
+        public HttpPatch(final String uri) {
+            super();
+            setURI(URI.create(uri));
+        }
 
         @Override
-        protected Boolean doInBackground(String... params) {
-            DefaultHttpClient client = new DefaultHttpClient();
-
-            String url = params[0];
-            HttpPatch patch = new HttpPatch(url);
-
-            StringEntity stringEntity = null;
-            try {
-                stringEntity = new StringEntity(params[1]);
-            } catch (UnsupportedEncodingException e) {
-                Log.w(TAG, e);
-                return false;
-            }
-
-            patch.setEntity(stringEntity);
-            patch.setHeader("Accept", "application/json");
-            patch.setHeader("Content-type", "application/json");
-
-            HttpResponse response = null;
-            try {
-                response = client.execute(patch);
-            } catch (IOException e) {
-                Log.w(TAG, e);
-                return false;
-            }
-
-            try {
-                String resp = EntityUtils.toString(response.getEntity());
-                Log.i(TAG, resp);
-            } catch (IOException e) {
-                Log.w(TAG, e);
-                return false;
-            }
-
-            return true;
-
-        }
-
-        protected void onPostExecute(String result) {
-
-        }
-
-        private class HttpPatch extends HttpEntityEnclosingRequestBase {
-
-            public final static String METHOD_NAME = "PATCH";
-
-            public HttpPatch() {
-                super();
-            }
-
-            public HttpPatch(final URI uri) {
-                super();
-                setURI(uri);
-            }
-
-            public HttpPatch(final String uri) {
-                super();
-                setURI(URI.create(uri));
-            }
-
-            @Override
-            public String getMethod() {
-                return METHOD_NAME;
-            }
-
+        public String getMethod() {
+            return METHOD_NAME;
         }
 
     }
